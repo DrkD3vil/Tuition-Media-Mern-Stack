8851015cb7b9a324ae30826412cbf77a43b1a6f7
BIJOY DEY <96878348+DrkD3vil@users.noreply.github.com>
2023-01-12 00:14:46 +0600

    node backend

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const app = express()
-const port = process.env.port || 5000
-
-app.listen(port, () => console.log(`Node Server Started at ${port}!`))
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const dbconfig = require("./config/dbconfig");
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Node Server Started at ${port}`));
